69634730268c3ab361f6ed6bbebb397b74ea2829
developer <developer@53b2abae7d86>
2023-12-11 08:32:07 +0000

    Base

--- src/Tarjeta.java
+++ src/Tarjeta.java
@@ -1,2 +1,31 @@
 public class Tarjeta {
+    private int saldoCreditos;
+    private int saldoTickets;
+    private int numeroTarjeta;
+    
+    public Tarjeta(int saldoCreditos, int saldoTickets) {
+        this.saldoCreditos = saldoCreditos;
+        this.saldoTickets = saldoTickets;
+        this.numeroTarjeta = (int) Math.random()*1000000000;
+    }
+
+    public int getSaldoCreditos() {
+        return saldoCreditos;
+    }
+
+    public int getSaldoTickets() {
+        return saldoTickets;
+    }
+
+    public int getNumeroTarjeta() {
+        return numeroTarjeta;
+    }
+
+    public void setSaldoCreditos(int saldoCreditos) {
+        this.saldoCreditos = saldoCreditos;
+    }
+
+    public void setSaldoTickets(int saldoTickets) {
+        this.saldoTickets = saldoTickets;
+    }
 }
--- src/clasePrincipal.java
+++ src/clasePrincipal.java
@@ -1,4 +1,5 @@
 public class clasePrincipal {
     public static void main(String[] args) {
+        
     }
 }
