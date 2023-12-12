1df1c31ec1ccbb39de5a0193304c748cc622a87c
developer <developer@8dfccc01afe7>
2023-12-12 08:16:15 +0000

    Tarjeta Inicial + test cases en main

--- src/Terminal.java
+++ src/Terminal.java
@@ -1,18 +1,13 @@
 
 /*
-Cada terminal contiene un lector de chips de tarjetas. 
-Un terminal acepta dinero que se convierte en créditos en una tarjeta. 
-El dinero se acepta como números enteros. 
-Los créditos se otorgan con una proporción de 2 créditos por cada euro. 
-Los jugadores pueden utilizar un terminal para consultar los saldos de sus tarjetas. 
-Incluya el número de la tarjeta en la impresión. 
-Todos los créditos o tickets, o parte de ellos, se pueden transferir de una tarjeta a otra. 
-Imprima siempre los saldos de una tarjeta cuando se acceda a los créditos o los tickets desde un terminal. 
-Por último, los tickets se pueden canjear en los terminales por premios. 
-Imprima un mensaje de error si una tarjeta no tiene suficientes tickets o 
-si el terminal no dispone de un determinado tipo de premio. 
-Imprima cuándo se concede un premio y el número restante de dicho tipo de premio en el terminal. 
-Los premios se canjean siempre a través de un terminal. Un terminal ofrece hasta tres premios.
+TODO: 
+CREAR SISTEMA DE CANJEO DE PREMIOS
+    IMPRIMIR MENSAJE ERROR SI PREMIO FUERA DE STOCK
+   
+LA TERMINAL SE INICIA CON UN CONSTRUCTOR QUE RECIBE 3 PREMIOS
+    LOS PREMIOS VENDRÁN DE UNA ARRAYLIST DINÁMICA BASADA EN LOS TIPOS DE PREMIOS DISPONIBLES
+    LA TERMINAL SÓLO PODRÁ DISPENSAR ESTE TIPO DE PREMIOS
+    LA TERMINAL RECIBE EL STOCK DEL PREMIO CON EL QUE SE CONSTRUYE
  */
 public class Terminal {
 
